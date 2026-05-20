8d948635d868e564d3f5b3b9630e547d3751f189
ChhayaC-star <chhaya.unihamburg@gmail.com>
2026-05-20 12:38:17 +0200

    Update ANOVA_20.05.2026.R

--- scripts/ANOVA_20.05.2026.R
+++ scripts/ANOVA_20.05.2026.R
@@ -32,3 +32,34 @@ summary(res.aov)
 
 # Tukey multiple pairwise- comparisons
 TukeyHSD(res.aov)
+
+
+#### Penguin data
+
+setwd("/Users/chhaya/Documents/Course/2026_summer")
+
+penguins <- read.csv("penguins.csv")
+
+levels <- c("male", "female")
+
+penguins$sex <- ordered(penguins$sex, levels = c("male", "female"))
+
+penguins_1 = penguins[-1,]
+
+group_by(penguins_1, sex) %>%
+  summarise(
+    count = n(),
+    mean = mean(body_mass_g, na.rm = TRUE),
+    sd = sd(body_mass_g, na.rm = TRUE)
+  )
+
+ggboxplot(penguins_1, x = "sex", y = "body_mass_g", 
+          color = "sex", palette = c("#00AFBB", "#E7B800"),
+          order = c("male", "female"),
+          ylab = "Body mass (gm)", xlab = "Sex")
+
+#compute ANOVA
+res.aov <- aov(body_mass_g ~ sex, data = penguins_1)
+summary(res.aov)
+
+TukeyHSD(res.aov)
