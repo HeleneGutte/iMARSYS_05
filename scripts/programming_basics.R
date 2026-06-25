15e9db0e29289318013b4bb3bed242dd65be9df4
HeleneGutte <helene.gutte@uni-hamburg.de>
2026-06-25 10:50:31 +0200

    Add solutions for the exercises of Day 10.

--- scripts/programming_basics.R
+++ scripts/programming_basics.R
@@ -40,20 +40,21 @@ View(fish_tl)
 # varies to the arguments:
 # Give your function a meaningful name
 
-name <- function(arguments){
-  body
+norm_species_length <- function(species){
+  (species - min(species, na.rm = TRUE)) / 
+    (max(species, na.rm = TRUE) - min(species, na.rm = TRUE))
 }
+
+norm_species_length(species = c(fish_tl$cod, fish_tl$herring, 
+                                fish_tl$plaice, fish_tl$flounder))
+
 
 # Step 3: Update your code
 fish_tl <- fish_tl %>%
-  mutate(cod_new = (cod - min(cod, na.rm = TRUE)) / 
-           (max(cod, na.rm = TRUE) - min(cod, na.rm = TRUE)),
-         herring_new = (herring - min(herring, na.rm = TRUE)) / 
-           (max(herring, na.rm = TRUE) - min(herring, na.rm = TRUE)),
-         plaice_new = (plaice - min(plaice, na.rm = TRUE)) / 
-           (max(plaice, na.rm = TRUE) - min(cod, na.rm = TRUE)),
-         flounder_new = (flounder - min(flounder, na.rm = TRUE)) / 
-           (max(flounder, na.rm = TRUE) - min(flounder, na.rm = TRUE))
+  mutate(cod_new = norm_species_length(species = cod),
+         herring_new = norm_species_length(species = herring),
+         plaice_new = norm_species_length(species = plaice),
+         flounder_new = norm_species_length(species = flounder)
   )
 
 
@@ -72,6 +73,8 @@ calc_cpue <- function(catches_in_lb, effort_in_h){
 
 calc_cpue(catches_in_lb = 10, effort_in_h = 0.5)
 x <- calc_cpue(catches_in_lb = 10, effort_in_h = 0.5)
+x
+
 
 # say you also want to have the effort in the output: 
 calc_cpue <- function(catches_in_lb, effort_in_h){
@@ -102,7 +105,7 @@ calc_cpue <- function(catches_in_lb, effort_in_h){
               cpue_kg_h = cpue))
 }
 
-calc_cpue(catches_in_lb = 10, effort_in_h = -0.5) # returns now an NA
+test <- calc_cpue(catches_in_lb = 10, effort_in_h = -0.5) # returns now an NA
 
 # add an if - else condition, e.g. a flag for high values:
 calc_cpue <- function(catches_in_lb, effort_in_h){
@@ -127,7 +130,7 @@ calc_cpue <- function(catches_in_lb, effort_in_h){
 }
 
 # add values: 
-calc_cpue(catches_in_lb, effort_in_h)
+calc_cpue(catches_in_lb = 10000, effort_in_h = 1)
 
 # Loops ----
 # Why do you think this doesn´t work? What does the error tell you? 
@@ -170,16 +173,22 @@ calc_cpue_advanced <- function(catches_in_lb, effort_in_h){
   if(effort_in_h <= 0){
     return(NA)
   } 
-  # if the if condition returns FALSEm the function simply continues here:
-  catch_in_kg <- catches_in_lb * 0.453592 
   
-  cpue <- catch_in_kg / effort_in_h
+  cpue <- rep(0, length(catches_in_lb))
+  status <- rep(0, length(catches_in_lb))
   
-  # indicate whether status of cpue is low or high
-  if(cpue > 50){
-    status <- "high"
-  } else{
-    status <- "low"
+  for(i in 1:length(catches_in_lb)){
+    # if the if condition returns FALSE the function simply continues here:
+    catch_in_kg <- catches_in_lb[i] * 0.453592 
+    
+    cpue[i] <- catch_in_kg / effort_in_h
+    
+    # indicate whether status of cpue is low or high
+    if(cpue[i] > 50){
+      status[i] <- "high"
+    } else{
+      status[i] <- "low"
+    }
   }
   
   return(list(effort_hours = effort_in_h, 
@@ -256,7 +265,118 @@ survey_length_data_2[survey_length_data_2 == 8] <- 0
 survey_length_data_2
 
 
+# Solutions ----
+# 1. Write a function to calculate biomass from length values 
+calc_biomass <- function(length_in_cm){
+  biomass <- 0.01 * length_in_cm ^ 3
+  biomass
+  # or alternatively: return(biomass)
+}
+# This function can currently calculate biomass for single values as well as 
+# vectors, because R recycles the equation for every entry in the vector.
+calc_biomass(length_in_cm = c(5, 7, 3))
+calc_biomass(5)
 
+# 2. Your colleague gave you his fish length data. While inspecting the data you 
+# saw that some entries are negative, ensure that the function returns NA, whenever
+# it hits a negative number. 
+# Use 9, 16, -2 as example values-
+calc_biomass <- function(length_in_cm){
+  # test for negative length values
+  if(length_in_cm < 0){
+    return(NA)
+  }
+  
+  biomass <- 0.01 * length_in_cm ^ 3
+  biomass
+  # or alternatively: return(biomass)
+}
+# No this function can handle only one value per time, because the if test, does
+# not know which of the values in the vector it should test.
+calc_biomass(9)
+calc_biomass(16)
+calc_biomass(-2)
+
+# 3. add to the output of your function a character value indicating whether the 
+# measured fish is small (<60) or big (>= 60)
+# try your own data fish_length_cm and the colleague´s data
+
+calc_biomass <- function(length_in_cm){
+  # test for negative length values
+  if(length_in_cm < 0){
+    return(NA)
+  }
+  
+  biomass <- 0.01 * length_in_cm ^ 3
+  if(biomass < 60){
+    size <- "small"
+  } else{
+    size <- "big"
+  }
+  data.frame(biomass = biomass, 
+             size = size)
+}
+calc_biomass(5)
+calc_biomass(7)
+calc_biomass(3)
+calc_biomass(9)
+calc_biomass(16)
+calc_biomass(-2)
+
+# 4. Since your function is working for your data and your colleagues data you
+# now want to evaluate all of the survey data: 
+survey_length_data <- rpois(100, 10)
+# Extend the function so that it can process all fish length at once. 
+survey_length_data < 0 
+# all values are positive, so we can skip the test for negative values.
+calc_biomass <- function(length_in_cm){
+  biomass <- rep(0, length(length_in_cm))
+  size <- rep(0, length(length_in_cm))
+  
+  for(i in 1:length(length_in_cm)){
+    biomass[i] <- 0.01 * length_in_cm[i] ^ 3
+    if(biomass[i] < 60){
+      size[i] <- "small"
+    } else{
+      size[i] <- "big"
+    }
+  }
+
+  data.frame(biomass = biomass, 
+             size = size)
+}
+
+calc_biomass(length_in_cm = survey_length_data)
+
+# 5. You get the data of a second survey, which has sometimes also enter 0, 
+# whenever it was not possible to measure a fish. Develop a solution how to 
+# handle the 0s in the data set. 
+survey_length_data_2 <- rpois(100, 10)
+survey_length_data_2[survey_length_data_2 == 8] <- 0
+survey_length_data_2
+calc_biomass <- function(length_in_cm){
+  biomass <- rep(0, length(length_in_cm))
+  size <- rep(0, length(length_in_cm))
+  
+  for(i in 1:length(length_in_cm)){
+    if(length_in_cm[i] <= 0){
+      biomass[i] <- NA
+      size[i] <- NA
+      next
+    }
+    biomass[i] <- 0.01 * length_in_cm[i] ^ 3
+    if(biomass[i] < 60){
+      size[i] <- "small"
+    } else{
+      size[i] <- "big"
+    }
+  }
+  
+  data.frame(biomass = biomass, 
+             size = size)
+}
+
+calc_biomass(survey_length_data_2)
 
 # Appendix ----
 # For loop with explanations as reminder: 
